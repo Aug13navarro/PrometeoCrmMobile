1d72bfe1ae8949b8e7a958a155a5df06a97e7e37
navarro.augusto.13 <navarro.augusto.13@gmail.com>
2023-11-28 00:04:58 -0300

    converter para label seller en pv

---
+++ UI/ValueConverters/LabelSellerOfOpportunityOrderNoteConverter.cs
@@ -0,0 +1,26 @@
+﻿using Core.Model;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace UI.ValueConverters
+{
+    public class LabelSellerOfOpportunityOrderNoteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var data = value as User;
+                return data.CodeFullName;
+            }
+
+            return "Sin Vendedor";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
